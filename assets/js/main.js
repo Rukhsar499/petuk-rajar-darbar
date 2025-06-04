471368df03a335232a6719dc39494824a09c18d4
Rukhsar499 <rukshar.digitalwolf@gmail.com>
2025-06-04 16:42:45 +0530

    hii

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,15 +86,7 @@ function topFunction() {
 
 
   AOS.init();
-  AOS.refresh(); // initialize AOS animations
-  $(document).ready(function(){
-      $("body,html").animate({
-          scrollTop: 10
-      }, 1000);
-      $("body,html").animate({
-          scrollTop: 0
-      }, 1);
-  });
+  
 
 // loader js
 var loader = document.getElementById("preloader");
